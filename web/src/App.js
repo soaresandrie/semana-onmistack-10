48d37b9b217cdf0ca17d991d449b5a98d1f49b24
soaresandrie <jsoaresandrie@gmail.com>
2020-01-16 23:37:40 -0300

    Adicionado a pasta source

---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//componenete - componenete é uma funçao que retorna um html ou css ou js
+//Estado -
+//Propriedade -
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+export default App;
